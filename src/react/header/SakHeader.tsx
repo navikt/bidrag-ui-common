b7eac3ef6017257ab1e95b403b532dac0bd03864
Ugur Cenar <cenar93@gmail.com>
2023-04-05 08:37:04 +0200

    Styling fix

--- src/react/header/SakHeader.tsx
+++ src/react/header/SakHeader.tsx
@@ -15,8 +15,8 @@ interface ISakHeaderProps {
 }
 export default function SakHeader({ saksnummer, roller, tittel }: ISakHeaderProps) {
     return (
-        <div className="bg-[var(--a-gray-50)] border-[var(--a-border-divider)] border-solid border-b w-full">
-            <div className="px-6 py-2 leading-10 flex items-center gap-x-4 border-[var(--a-border-divider)] border-solid border-b">
+        <div className="bg-[var(--a-gray-50)] border-[var(--a-border-divider)] border-solid border-b w-full border-0">
+            <div className="px-6 py-2 leading-10 flex items-center gap-x-4 border-[var(--a-border-divider)] border-solid border-b border-0">
                 {tittel ? (
                     <>
                         <Heading level="1" size="xlarge">
@@ -29,15 +29,16 @@ export default function SakHeader({ saksnummer, roller, tittel }: ISakHeaderProp
                 )}
             </div>
 
-            <Grid>
-                <Cell xs={12} sm={8} md={9} lg={3}>
+            {/** @ts-ignore **/}
+            <Grid className={"max-w-5xl"}>
+                <Cell xs={12} sm={10} md={9} lg={5}>
                     {roller
                         ?.filter((r) => r.rolleType != RolleType.BA)
                         .map((rolle, i) => (
                             <RolleDetaljer key={rolle.ident + i} rolle={rolle} withBorder={false} />
                         ))}
                 </Cell>
-                <Cell xs={12} md={8} lg={6}>
+                <Cell xs={12} md={10} lg={5}>
                     {roller
                         ?.filter((r) => r.rolleType == RolleType.BA)
                         .map((rolle, i) => (
