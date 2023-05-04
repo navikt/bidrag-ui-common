9fc0d021d607eefbfcc97471ec612a86519fff7c
Ugur Cenar <cenar93@gmail.com>
2023-05-04 14:59:49 +0200

    fix: Sakheader styling fix

--- src/react_components/header/SakHeader.tsx
+++ src/react_components/header/SakHeader.tsx
@@ -1,4 +1,4 @@
-import { Heading } from "@navikt/ds-react";
+import { BodyShort } from "@navikt/ds-react";
 import { Cell } from "@navikt/ds-react";
 import { Grid } from "@navikt/ds-react";
 import { CopyToClipboard } from "@navikt/ds-react-internal";
@@ -8,15 +8,22 @@ import { IRolleDetaljer } from "../../types/roller/IRolleDetaljer";
 import { RolleType } from "../../types/roller/RolleType";
 import { RolleDetaljer } from "../roller/RolleDetaljer";
 
+interface ISkjermbildeDetaljer {
+    navn: string;
+    referanse: string;
+}
 interface ISakHeaderProps {
     saksnummer: string;
-    tittel?: string;
     roller: IRolleDetaljer[];
+    skjermbilde?: ISkjermbildeDetaljer;
 }
-export default function SakHeader({ saksnummer, roller, tittel }: ISakHeaderProps) {
+export default function SakHeader({ saksnummer, roller, skjermbilde }: ISakHeaderProps) {
     return (
         <div className="bg-[var(--a-gray-50)] border-[var(--a-border-divider)] border-solid border-b w-full border-0">
             {/** @ts-ignore **/}
+            <div className="px-6 py-1 flex items-center border-[var(--a-border-divider)] border-solid border-b border-0">
+                <SkjermbildeDetaljer saksnummer={saksnummer} skjermbilde={skjermbilde} />
+            </div>
             <Grid className={"max-w-5xl"}>
                 <Cell xs={12} sm={10} md={9} lg={6}>
                     {roller
@@ -37,11 +44,24 @@ export default function SakHeader({ saksnummer, roller, tittel }: ISakHeaderProp
     );
 }
 
-function Saksnummer({ saksnummer, isSmall }: { saksnummer: string; isSmall?: boolean }) {
+function SkjermbildeDetaljer({ saksnummer, skjermbilde }: { saksnummer: string; skjermbilde?: ISkjermbildeDetaljer }) {
     return (
-        <span className="text-base flex items-center font-normal">
-            <Heading size={isSmall ? "medium" : "large"}>Saksnr. {saksnummer}</Heading>
-            <CopyToClipboard size="small" copyText={saksnummer} popoverText="Kopierte saksnummer" />
-        </span>
+        <div className="flex flex-row">
+            <span className="text-base flex items-center font-normal">
+                <BodyShort size={"small"}>Saksnr. {saksnummer}</BodyShort>
+                <CopyToClipboard size="xsmall" copyText={saksnummer} popoverText="Kopierte saksnummer" />
+            </span>
+            {skjermbilde && (
+                <>
+                    <div className="pl-1 pr-1">/</div>
+                    <span className="text-base flex items-center font-normal">
+                        <BodyShort size={"small"}>
+                            {skjermbilde.navn} {skjermbilde.referanse}
+                        </BodyShort>
+                        <CopyToClipboard size="xsmall" copyText={skjermbilde.referanse} popoverText="Kopiert" />
+                    </span>
+                </>
+            )}
+        </div>
     );
 }
--- src/react_components/roller/RolleDetaljer.tsx
+++ src/react_components/roller/RolleDetaljer.tsx
@@ -14,13 +14,13 @@ export const RolleDetaljer = ({ rolle, withBorder = true }: IRolledetaljerProps)
     return (
         <BodyShort
             size="small"
-            className={`px-8 py-2 ${withBorder && "border-[var(--a-border-divider)] border-solid border-b"
+            className={`px-6 py-1 ${withBorder && "border-[var(--a-border-divider)] border-solid border-b"
                 } flex items-center`}
         >
             <RolleTag rolleType={rolle.rolleType} />
             <span>{rolle.navn}</span>
             <span className="mx-1">/</span> {rolle.ident}
-            <CopyToClipboard size="small" copyText={rolle.ident} popoverText="Kopiert til utklippstavlen" />
+            <CopyToClipboard size="xsmall" copyText={rolle.ident} popoverText="Kopiert til utklippstavlen" />
         </BodyShort>
     );
 };
