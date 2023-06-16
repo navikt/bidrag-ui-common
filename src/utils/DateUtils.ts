c4689472e9dbaef744504f5e55a9a35d95232b3c
markodraksic <draksic.m@gmail.com>
2023-06-16 09:50:25 +0200

    Edit date-utils, remove dayjs

--- src/utils/DateUtils.ts
+++ src/utils/DateUtils.ts
@@ -1,36 +1,32 @@
-import dayjs from "dayjs";
-import customParseFormat from "dayjs/plugin/customParseFormat";
-
-import ObjectUtils from "./ObjectUtils";
-
-dayjs.extend(customParseFormat);
-
-const DATE_FORMATS = ["DDMMYY", "DD.MM.YYYY", "YYYY-MM-DD"];
-export const isValidDate = (date: string, formats: string[] | string = DATE_FORMATS) => {
-    if (ObjectUtils.isEmpty(date)) {
-        return true;
-    }
-
-    return dayjs(date, formats, true).isValid();
+export const dateToDDMMYYYYString = (date: Date) =>
+    date.toLocaleDateString("nb-NO", { year: "numeric", month: "2-digit", day: "2-digit" });
+export const toISODateString = (date: Date): string =>
+    date.toLocaleDateString("sv-SV", { year: "numeric", month: "2-digit", day: "2-digit" });
+export const addDays = (date: Date, days: number) => {
+    const newDate = new Date(date);
+    newDate.setDate(newDate.getDate() + days);
+    return newDate;
 };
-
-export const isFutureDate = (date: string) => {
-    if (!isValidDate(date, DATE_FORMATS)) {
+export const deductDays = (date: Date, days) => {
+    const newDate = new Date(date);
+    newDate.setDate(newDate.getDate() - days);
+    return newDate;
+};
+export const lastDayOfMonth = (date: Date) => new Date(date.getFullYear(), date.getMonth() + 1, 0);
+export const firstDayOfMonth = (date: Date) => new Date(date.getFullYear(), date.getMonth(), 1);
+export const isValidDate = (date: any) => date && date instanceof Date && isFinite(date.getTime());
+export const isFirstDayOfMonth = (date: Date) => {
+    const firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
+    return firstDay.getDate() === date.getDate();
+};
+export const isLastDayOfMonth = (date: Date) => {
+    const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+    return lastDay.getDate() === date.getDate();
+};
+export const isAfterDate = (date: string, maxValidate: string) => {
+    if (!isValidDate(new Date(date))) {
         return false;
     }
-    return dayjs(date, DATE_FORMATS, true).isAfter(dayjs(new Date()), "days");
+    return new Date(date) > new Date(maxValidate);
 };
-
-export const isAfterDate = (date: string, maxValidDate: string) => {
-    if (!isValidDate(date, DATE_FORMATS)) {
-        return false;
-    }
-    return dayjs(date, DATE_FORMATS, true).isAfter(dayjs(maxValidDate, "YYYY-MM-DD"), "days");
-};
-
-export const formatDate = (date: string, format = "DD.MM.YYYY") => {
-    if (isValidDate(date, DATE_FORMATS) && !ObjectUtils.isEmpty(date)) {
-        return dayjs(date, DATE_FORMATS, true).format(format);
-    }
-    return date;
-};
+export const isFutureDate = (date: string) => isAfterDate(date, toISODateString(new Date()));
