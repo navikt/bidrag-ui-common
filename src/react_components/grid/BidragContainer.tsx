8a64c7ce0c8a1f4978e26fecbb9cfb957c5a4eaa
markodraksic <draksic.m@gmail.com>
2023-07-12 17:01:22 +0200

    Edit grid css

--- src/react_components/grid/BidragContainer.tsx
+++ src/react_components/grid/BidragContainer.tsx
@@ -1,8 +1,9 @@
 import { PropsWithChildren } from "react";
+
 type ContainerProps = React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
 export default function BidragContainer({ children, ...otherProps }: PropsWithChildren<ContainerProps>) {
     return (
-        <div {...otherProps} className={`m-auto max-w-[1096px] ${otherProps.className}`}>
+        <div {...otherProps} className={`container mx-auto ${otherProps.className}`}>
             {children}
         </div>
     );
--- src/react_components/grid/BidragGrid.tsx
+++ src/react_components/grid/BidragGrid.tsx
@@ -1,8 +1,9 @@
 import { PropsWithChildren } from "react";
+
 type GridProps = React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
 export default function BidragGrid({ children, ...otherProps }: PropsWithChildren<GridProps>) {
     return (
-        <div {...otherProps} className={`grid md:grid-cols-auto grid-cols-autosmall gap-1 ${otherProps.className}`}>
+        <div {...otherProps} className={`grid grid-cols-12 gap-6 ${otherProps.className}`}>
             {children}
         </div>
     );
