40a7b4bde940aaf7cd6dbda8619d4a9bdbbefd45
markodraksic <draksic.m@gmail.com>
2023-08-23 09:27:07 +0200

    Edit e-chart storybook to trigger deployment

--- src/stories/Chart.stories.ts
+++ src/stories/Chart.stories.ts
@@ -8,7 +8,6 @@ const meta = {
     // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
     tags: ["autodocs"],
     parameters: {
-        // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
         layout: "fullscreen",
     },
 } satisfies Meta<typeof EChartLineChart>;
