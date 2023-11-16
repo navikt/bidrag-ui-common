393540847f211038355b4ff2f810d71e30890a7c
Ugur Cenar <cenar93@gmail.com>
2023-11-16 11:49:16 +0100

    fix: Bugfix useRQMutationState and add SaveStatusIndicator component

--- src/react_components/hooks/useRQMutationState.ts
+++ src/react_components/hooks/useRQMutationState.ts
@@ -1,21 +1,30 @@
-import { MutationStatus, useQueryClient } from "@tanstack/react-query";
+import { MutationStatus, QueryClient, useQueryClient } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 
-export function useRQMutationState(compareToKey: string | string[]) {
-    const [saveState, setSaveState] = useState<MutationStatus | undefined>();
-    const queryClient = useQueryClient();
+export function useRQMutationState(compareToKey?: any | any[], _queryClient?: QueryClient) {
+    const [saveState, setSaveState] = useState<MutationStatus>("idle");
+    const queryClient = useQueryClient(_queryClient);
 
     useEffect(() => {
+        if (compareToKey == undefined) return;
+
         const unsubscribe = queryClient.getMutationCache().subscribe((cache) => {
+            if (cache.type == "observerOptionsUpdated") return;
             const mutationKey = cache.mutation?.options.mutationKey;
             const mutationKeyList = mutationKey
-                ? typeof mutationKey == "string"
+                ? typeof mutationKey != "object"
                     ? [mutationKey]
                     : (mutationKey as string[])
                 : [];
+            const mutationStatus = cache.mutation?.state?.status;
+            const status = mutationStatus == undefined ? "idle" : mutationStatus;
             const isSameMutation = mutationKeyList.every((key) => compareToKey.includes(key));
-            if (isSameMutation) {
-                setSaveState(cache.mutation?.state?.status);
+            if (cache?.mutation) {
+                console.debug(cache, isSameMutation, mutationKey, compareToKey);
+            }
+            const hasChanged = status !== saveState;
+            if (isSameMutation && hasChanged) {
+                setSaveState(status);
             }
         });
         return () => unsubscribe();
