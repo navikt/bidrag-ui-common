e75c1c3ea446e95ca0c82c3f18a94cce243d6d52
Ugur Alpay Cenar <cenar93@gmail.com>
2023-11-16 11:56:55 +0100

    fix: Bugfix useRQMutationState and add SaveStatusIndicator component (#156)

---
+++ src/react_components/SaveStatusIndicator.tsx
@@ -0,0 +1,54 @@
+import { CheckmarkCircleIcon, XMarkOctagonIcon } from "@navikt/aksel-icons";
+import { BodyShort, Loader } from "@navikt/ds-react";
+import { MutationStatus, QueryClient } from "@tanstack/react-query";
+
+import { useRQMutationState } from "./hooks";
+type SaveStatusIndicatorProps = {
+    mutationKey?: any[] | any;
+    state?: MutationStatus;
+    queryClient?: QueryClient;
+};
+export default function SaveStatusIndicator({
+    state: stateInput,
+    mutationKey,
+    queryClient: _queryClient,
+}: SaveStatusIndicatorProps) {
+    const saveState = useRQMutationState(mutationKey, _queryClient);
+    const state = stateInput ?? saveState;
+    function renderContent() {
+        if (state == "error") {
+            return (
+                <div className="inline-flex text-nav-red gap-[3px]">
+                    <XMarkOctagonIcon /> <BodyShort size="small">Lagring feilet</BodyShort>
+                </div>
+            );
+        }
+        if (state == "pending") {
+            return (
+                <div className="inline-flex gap-[3px]">
+                    <Loader size="xsmall" title="Lagrer" />
+                    <BodyShort size="small">Lagrer...</BodyShort>
+                </div>
+            );
+        }
+        return (
+            <div className="inline-flex gap-[3px]">
+                <div className="text-green-500">
+                    <CheckmarkCircleIcon />
+                </div>{" "}
+                <BodyShort size="small">Lagret</BodyShort>
+            </div>
+        );
+    }
+
+    function getStyles() {
+        if (state == "error") {
+            return `border-border-danger pr-1 pl-1 w-[122px]`;
+        }
+        if (state == "pending") {
+            return `border-border-info w-[70px]`;
+        }
+        return `transition-[width] ease-in-out border-border-success duration-300 w-[65px]`;
+    }
+    return <div className={`${getStyles()} self-center`}>{renderContent()}</div>;
+}
--- src/react_components/index.ts
+++ src/react_components/index.ts
@@ -8,3 +8,4 @@ export * from "./hooks";
 export { default as PersonSokButton } from "./person/PersonSokButton";
 export { default as RolleDetaljer } from "./roller/RolleDetaljer";
 export { default as RolleTag } from "./roller/RolleTag";
+export { default as SaveStatusIndicator } from "./SaveStatusIndicator";
