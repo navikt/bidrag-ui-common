41a0b7c9ce3767604da954fd7d554acfe3cc3c25
Ugur Alpay Cenar <cenar93@gmail.com>
2025-07-22 10:58:48 +0200

    Legg til traceheadere for logging (#361)

--- src/logging/LoggerService.ts
+++ src/logging/LoggerService.ts
@@ -3,13 +3,14 @@ import { LogInfo } from "../types";
 import { LogResponse } from "../types";
 
 export class LoggerService extends AbstractLoggerService {
-    static override log(logInfo: LogInfo): Promise<LogResponse> {
+    static override log(logInfo: LogInfo, headers?: Record<string, string>): Promise<LogResponse> {
         return fetch("/log", {
             mode: "cors",
             cache: "no-cache",
             body: JSON.stringify(logInfo),
             method: "POST",
             headers: {
+                ...headers,
                 "Content-type": "application/json; charset=UTF-8",
             },
         })
