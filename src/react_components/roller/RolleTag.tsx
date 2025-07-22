04a516bc54bb0415bdac30470dcd59b35e4c3180
Ugur Alpay Cenar <cenar93@gmail.com>
2025-07-22 14:38:03 +0200

    fix: Refactor personnavnident (#364)

--- src/react_components/roller/RolleTag.tsx
+++ src/react_components/roller/RolleTag.tsx
@@ -6,7 +6,7 @@ import { RolleType } from "../../types/roller/RolleType";
 const RolleTag = ({ rolleType }: { rolleType: RolleType }) => {
     return (
         // @ts-ignore
-        <Tag variant={ROLE_TAGS[rolleType]} size="small" className="w-8 mr-2 rounded">
+        <Tag variant={ROLE_TAGS[rolleType]} size="small" className={`w-8 mr-2 rounded select-none rolleTag ${rolleType}`}>
             {/** @ts-ignore */}
             {ROLE_FORKORTELSER[rolleType] ?? rolleType}
         </Tag>
