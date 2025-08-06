bc439c39c6f48011e54ee03b6bbd477fc7757b2b
Ugur Alpay Cenar <cenar93@gmail.com>
2025-08-06 16:56:49 +0200

    fix: Styling personnavnident (#372)

--- src/react_components/roller/RolleTag.tsx
+++ src/react_components/roller/RolleTag.tsx
@@ -3,10 +3,14 @@ import { Tag } from "@navikt/ds-react";
 import { ROLE_FORKORTELSER, ROLE_TAGS } from "../../types/roller/RoleTags";
 import { RolleType } from "../../types/roller/RolleType";
 
-const RolleTag = ({ rolleType }: { rolleType: RolleType }) => {
+const RolleTag = ({ rolleType, className }: { rolleType: RolleType; className?: string }) => {
     return (
         // @ts-ignore
-        <Tag variant={ROLE_TAGS[rolleType]} size="small" className={`w-8 mr-2 rounded select-none rolleTag ${rolleType}`}>
+        <Tag
+            variant={ROLE_TAGS[rolleType]}
+            size="small"
+            className={`w-8 mr-2 rounded select-none rolleTag ${rolleType} ${className}`}
+        >
             {/** @ts-ignore */}
             {ROLE_FORKORTELSER[rolleType] ?? rolleType}
         </Tag>
