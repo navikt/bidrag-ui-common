accd9a3b0464893827beed26f1c349efd86f4e12
markodraksic <draksic.m@gmail.com>
2025-09-16 14:31:39 +0200

    Fix usage of useApi

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,9 +1,8 @@
 import { useApi } from "../react_components/hooks";
 import { Api as PersonApi } from "./PersonApi";
 
-export const PERSON_API = useApi(
-    new PersonApi({ baseURL: process.env.BIDRAG_PERSON_URL }),
-    "bidrag-person",
-    "fss",
-    process.env.ENVIRONMENT
-);
+export const PERSON_API = useApi(new PersonApi({ baseURL: process.env.BIDRAG_PERSON_URL }), {
+    app: "bidrag-person",
+    cluster: "fss",
+    env: process.env.ENVIRONMENT,
+});
