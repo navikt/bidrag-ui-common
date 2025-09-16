aa30ecfe74091f6a242615a55ba8b3931f42b161
Ugur Alpay Cenar <cenar93@gmail.com>
2025-09-16 09:56:00 +0200

    fix: Feilfiks diskresjonskode (#384)
    * fix: Mask sensitiv info listener
    
    * fix: Vis bare diskresjonskode for kode6/7
    
    * Fiks byggefeil

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,9 @@
 import { useApi } from "../react_components/hooks";
 import { Api as PersonApi } from "./PersonApi";
 
-export const PERSON_API = useApi(new PersonApi({ baseURL: process.env.BIDRAG_PERSON_URL }), "bidrag-person", "fss");
+export const PERSON_API = useApi(
+    new PersonApi({ baseURL: process.env.BIDRAG_PERSON_URL }),
+    "bidrag-person",
+    "fss",
+    process.env.ENVIRONMENT
+);
--- src/api/useApiData.ts
+++ src/api/useApiData.ts
@@ -1,7 +1,7 @@
 import { useSuspenseQuery } from "@tanstack/react-query";
 
 import { PERSON_API } from "./api";
-import { PersonDto } from "./PersonApi";
+import { Graderingsinfo, PersonDto } from "./PersonApi";
 
 const getKey = (ident?: string) => ["persons", ident ?? "ukjent"];
 
@@ -11,6 +11,17 @@ export const useHentPersonData = (ident?: string) => {
         queryFn: async (): Promise<PersonDto> => {
             if (!ident) return { ident: "", visningsnavn: "Ukjent" };
             const { data } = await PERSON_API.informasjon.hentPersonPost({ ident: ident });
+            return data;
+        },
+        staleTime: Infinity,
+    });
+};
+export const useHentPersonSkjermingInfo = (ident?: string) => {
+    return useSuspenseQuery({
+        queryKey: getKey(ident),
+        queryFn: async (): Promise<Graderingsinfo> => {
+            if (!ident) return { identerTilGradering: {}, identerTilSkjerming: {} };
+            const { data } = await PERSON_API.graderingsinfo.hentGraderinger([ident]);
             return data;
         },
         staleTime: Infinity,
