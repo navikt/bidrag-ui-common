091841240e513735fa98d3510179a5cd42ceaca6
natchananthawisit <natcha.nanthawisit@nav.no>
2025-11-21 14:25:40 +0100

    feat: ryddet MaskerSensitivInfo

--- src/react_components/MaskerSensitivInfo.tsx
+++ src/react_components/MaskerSensitivInfo.tsx
@@ -8,7 +8,6 @@ type MaskerSensitivInfoProps = {
 
 export default function MaskerSensitivInfo({ children, className = "" }: MaskerSensitivInfoProps) {
     const { erMaskert } = useBidragCommons();
-
     const maskeringsKlasser = erMaskert ? "blur-[5px] select-none pointer-events-none" : "";
 
     return <div className={`${className} ${maskeringsKlasser}`}>{children}</div>;
