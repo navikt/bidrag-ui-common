63848ba55629429d8e2a454bc9e36f86c0be4247
Ugur Alpay Cenar <cenar93@gmail.com>
2026-03-10 08:51:41 +0100

    fix: Info om ny versjon (#453)

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export { default as BidragCommonsProvider } from "./api/BidragCommonsContext";
 export * from "./docs";
 export * from "./logging";
 export * from "./react_components";
+export { default as MfeVersionChecker } from "./react_components/MfeVersionChecker";
 export * from "./service/DefaultRestService";
 export * from "./types";
 export * from "./utils";
